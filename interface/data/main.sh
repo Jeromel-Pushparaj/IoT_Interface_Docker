0b1ac10039ed56110b7faaf80b36dfcd6e674530
Jeromel Pushparaj <jeromal03@gmail.com>
2025-02-12 18:03:18 +0530

    Fix Shell Script

--- interface/data/main.sh
+++ interface/data/main.sh
@@ -1,8 +1,5 @@
+touch /var/interface/dockerexpirience
 
-
-touch /var/photogram/dockerexpirience
-
-# git clone https://git.selfmade.ninja/jeromal03/first_app.git /var/www/html
-# cp /var/photogram/photogramconfig.json /var/www/
 sed -i "s/short_open_tag =.*/short_open_tag = On/" /etc/php/8.3/apache2/php.ini
+
 /usr/sbin/apache2ctl -D FOREGROUND
