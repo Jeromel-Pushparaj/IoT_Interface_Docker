268f0565903fdabc09b79f8f007dde80e6b402b0
Jeromel Pushparaj <jeromal03@gmail.com>
2025-02-17 22:32:31 +0530

    Fix Docker

--- interface/data/main.sh
+++ interface/data/main.sh
@@ -1,7 +1,7 @@
 touch /var/interface/dockerexpirience
 
 sed -i "s/short_open_tag =.*/short_open_tag = On/" /etc/php/8.3/apache2/php.ini
-
+ 
 # Run Certbot to obtain SSL certificate (use --non-interactive to avoid manual input)
 # certbot --apache --non-interactive --agree-tos -m jeromal03@gmail.com -d iotinterface.site
 
