4bb4ba471910b371a309ef069fb87c9a42b74ce0
Jeromel Pushparaj <jeromal03@gmail.com>
2025-03-09 21:09:50 +0530

    Add MongoDB to Compose and Mongodb extensions to image

--- interface/data/main.sh
+++ interface/data/main.sh
@@ -5,4 +5,8 @@ sed -i "s/short_open_tag =.*/short_open_tag = On/" /etc/php/8.3/apache2/php.ini
 # Run Certbot to obtain SSL certificate (use --non-interactive to avoid manual input)
 # certbot --apache --non-interactive --agree-tos -m jeromal03@gmail.com -d iotinterface.site
 
+cd /var/www/html/htdocs
+composer require mongodb/mongodb
+# composer require vlucas/phpdotenv
+
 /usr/sbin/apache2ctl -D FOREGROUND
